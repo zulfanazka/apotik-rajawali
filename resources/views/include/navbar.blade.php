82b8d114357b443c8528f576842fda369896abe9
akhmad35 <email-akhmad35@example.com>
2025-05-25 19:43:08 +0700

    p

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,5 +1,8 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+<a class="navbar-brand" href="index.html">
+    <img src="{{ asset('template/assets/img/logo.png') }}" alt="Logo" style="height: 40px;">
+</a>
+
     <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#">
         <i class="fas fa-bars"></i>
     </button>
